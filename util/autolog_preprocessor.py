113accdd247e38170333cab12fb13c1343e55f20
Fiete Minge <fietensen@gmail.com>
2025-03-01 13:44:34 +0100

    impl basic autlog scoring system

--- util/autolog_preprocessor.py
+++ util/autolog_preprocessor.py
@@ -2,14 +2,18 @@
 Handles the parsing of Logs as described in the AutoLog paper
 """
 
-from pathlib import Path
+from sklearn.feature_extraction.text import CountVectorizer
+from collections import defaultdict, OrderedDict
 from typing import Callable, Iterator
+from pathlib import Path
+
 import multiprocessing as mp
-import parse_util as pu
-import datetime
+import util.parse_util as pu
 import pandas as pd
-import re
+import numpy as np
+import datetime
 import os
+import re
 
 # Parses line from BG/L Logfile
 BGL_LINE_RE = re.compile(
@@ -35,7 +39,6 @@ HDFS_LINE_RE = re.compile(
 
 # Matches first blk entry in logfile
 HDFS_BLK_RE = re.compile(r"^.*\s(blk_-?\d+)\s.*$")
-
 
 def _process_bgl_line(line: str) -> tuple[str]:
     proc = pu.process_line(
@@ -66,11 +69,12 @@ def _process_bgl_line(line: str) -> tuple[str]:
         pass
 
     return tuple([
-        (proc[0] != "-"),  # Anomalous
-        le,                # Log Entity
-        time_parsed,       # Time
-        cont_parsed        # Content
-    ]), None
+        (proc[0] != "-"),         # Anomalous
+        le,                       # Log Entity
+        time_parsed.timestamp(),  # Time
+        cont_parsed,              # Content
+        None
+    ])
 
 
 def _process_hdfs_line(line: str) -> tuple[str]:
@@ -87,7 +91,7 @@ def _process_hdfs_line(line: str) -> tuple[str]:
         proc[0], "%y%m%d %H%M%S"
     )
 
-    le = proc[1] + "-" + proc[2]
+    le = proc[1] + "-" + proc[2] # TODO: Should this use the PID?
     cont_parsed = pu.format_text(proc[3])
     blk_id = HDFS_BLK_RE.match(proc[3])
     if not blk_id:
@@ -96,13 +100,13 @@ def _process_hdfs_line(line: str) -> tuple[str]:
     return tuple([
         False,
         le,
-        time_parsed,
+        time_parsed.timestamp(),
         cont_parsed,
         blk_id.groups()[0]
     ])
 
 
-def _process_batch(lines: list, line_processor: Callable) -> list:
+def _process_loglines_batch(lines: list, line_processor: Callable) -> list:
     return [line_processor(line) for line in lines]
 
 
@@ -131,21 +135,20 @@ def _process_logfile(path: Path, line_processor: Callable, print_progress: bool 
 
     with mp.Pool(processes=mp.cpu_count()) as pool:
         results = [pool.apply_async(
-            _process_batch, (batch, line_processor)) for batch in log_generator(path)]
+            _process_loglines_batch, (batch, line_processor)) for batch in log_generator(path)]
         processed_batches = [r.get() for r in results]
 
     for processed_batch in processed_batches:
         df_list.append(pd.DataFrame(processed_batch, columns=[
                        "anomalous", "log_entity", "timestamp", "message", "ext"]))
 
-    df = pd.concat(df_list, ignore_index=True).dropna()
+    df = pd.concat(df_list, ignore_index=True).dropna(how="all")
     df.index.name = "index"
     df["log_entity"] = df["log_entity"].astype(str)
 
     return df
 
-
-def _set_hdfs_labels(df: pd.DataFrame, label_file: Path) -> None:
+def _set_hdfs_labels(df: pd.DataFrame, label_file: Path) -> pd.DataFrame:
     an = pd.read_csv(label_file).set_index("BlockId").to_dict()["Label"]
     df["anomalous"] = df["ext"].map(lambda blk_id: an[blk_id] == "Anomaly")
 
@@ -159,6 +162,7 @@ def prepare_hdfs(hdfs_dataset_path: Path) -> None:
     if os.path.isfile(hdfs_log_parsed):
         print("[*] Loading cached preprocessed file from " + str(hdfs_log_parsed))
         parsed = pd.read_csv(hdfs_log_parsed)
+        parsed.index.name = "index"
 
     else:
         parsed = _process_logfile(
@@ -170,12 +174,12 @@ def prepare_hdfs(hdfs_dataset_path: Path) -> None:
         _set_hdfs_labels(parsed, hdfs_label_file)
         print("[INFO] Caching progress to " + str(hdfs_log_parsed))
         del parsed["ext"]
-        parsed.to_csv(hdfs_log_parsed)
+        parsed.to_csv(hdfs_log_parsed, index=False)
 
-    print(parsed.head())
+    return parsed
 
 
-def prepare_bgl(bgl_dataset_path: Path) -> None:
+def prepare_bgl(bgl_dataset_path: Path) -> pd.DataFrame:
     bgl_log_file = bgl_dataset_path / "BGL.log"
     bgl_log_parsed = bgl_dataset_path / "BGL.prep.csv"
 
@@ -184,6 +188,8 @@ def prepare_bgl(bgl_dataset_path: Path) -> None:
     if os.path.isfile(bgl_log_parsed):
         print("[INFO] Loading cached preprocessed file from " + str(bgl_log_parsed))
         parsed = pd.read_csv(bgl_log_parsed)
+        parsed.index.name = "index"
+        parsed["log_entity"] = parsed["log_entity"].replace(np.nan, "NULL")
 
     else:
         parsed = _process_logfile(
@@ -193,9 +199,48 @@ def prepare_bgl(bgl_dataset_path: Path) -> None:
         )
         print("[INFO] Caching progress to " + str(bgl_log_parsed))
         del parsed["ext"]
-        parsed.to_csv(bgl_log_parsed)
+        parsed.to_csv(bgl_log_parsed, index=False)
 
-    print(parsed.head())
+    return parsed
+
+def mk_chunks_vectorizers(df: pd.DataFrame, time_interval: int = 10) -> tuple[dict[str, CountVectorizer], pd.DataFrame, pd.DataFrame]:# -> tuple[dict[str, dict[int, int]], dict[str, CountVectorizer], dict[str, pd.DataFrame]]:
+    ts_start = df["timestamp"].min()
+
+    df["tblk"] = ((df["timestamp"]-ts_start)//time_interval).astype(int)
+    tblk_groups = df.groupby("tblk")
+    df["anomalous"] = tblk_groups["anomalous"].transform("any")
+
+    le_vectorizers = defaultdict(lambda:CountVectorizer(token_pattern=r"(?u)\b\w\w*\b"))
+    norm_df = df[df["anomalous"] == False]
+    anom_df = df[df["anomalous"] == True]
+
+    for le_name, gdf in norm_df.groupby("log_entity"):
+        le_vectorizers[le_name].fit([" ".join(gdf["message"].values)])
+
+    return le_vectorizers, norm_df, anom_df
+
+def calc_term_entropies(vectorizer: CountVectorizer, le_messages: pd.Series, M: int) -> np.array:
+    sp_mat = vectorizer.transform(le_messages)
+
+    # e_t = 1 + 1/log2(M) * (\sum_{j}^{M} p_t_j*log2(p_t_j))
+    abs_sum = np.asarray(sp_mat.sum(axis=0)).flatten()
+    abs_sum[abs_sum == 0] = 1 # avoid division by zero
+    norm_sp_mat = sp_mat.multiply(1 / abs_sum)
+    p_log_p = norm_sp_mat.copy()
+    p_log_p.data = p_log_p.data * np.log2(p_log_p.data)
+    entropy_t = np.asarray(p_log_p.sum(axis=0)).flatten()
+    H_t = 1 + (entropy_t / np.log2(M))
+    
+    return H_t
+
+def calc_chunk_scores(vectorizer: CountVectorizer, entropies: np.array, chunks: list[str]) -> np.array:
+    sp_mat = vectorizer.transform(chunks)
+    sp_mat.data = np.log2(1 + sp_mat.data)
+    sp_mat = sp_mat.multiply(entropies).power(2).sum(axis=1).flatten()
+
+    chunk_scores = np.sqrt(sp_mat)
+
+    return chunk_scores
 
 
 if __name__ == '__main__':
